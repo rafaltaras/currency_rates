eb26e4fde1832d68ede21492627fb8971cf13e80
Taraska Rafał - Korpo <Rafal.Taraska@orange.com>
2022-12-07 14:15:23 +0100

    v1.0

---
+++ app.py
@@ -0,0 +1,52 @@
+import requests
+import json
+import pickle
+import csv
+
+res = requests.get("http://api.nbp.pl/api/exchangerates/tables/C?format=json")
+data = res.json()
+data_as_dict = data[0]
+# data_rates = data_as_dict.get('rates')
+data_rates_as_json = json.dumps(data_as_dict)
+data_from_json = json.loads(data_rates_as_json)
+
+data_rates = data_from_json.get('rates')
+
+def get_rates(data): 
+    for i in data:
+        for j in i:
+            print(j, end=';')   
+        break
+    print(end='\n')
+   
+    for item in data:
+        for value in item.values():
+            print(value, end=';')
+        print(end='\n') 
+        
+
+rates = get_rates(data_rates)
+
+print(type(rates))
+
+
+
+
+# data_for_csv += data_for_csv.append(data)
+# print(type(data_for_csv))
+
+
+# with open('rates.csv', 'w', newline='') as file:
+#     writer = csv.writer(file)
+#     writer.writerow(rates)
+
+# with open('countries.csv', 'w', encoding='UTF8') as f:
+#     writer = csv.writer(f)
+
+#     # write the header
+#     writer.writerow(header)
+
+#     # write the data
+#     writer.writerow(data)
+
+
